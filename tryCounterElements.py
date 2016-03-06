5d270346f690843be7e0cf472fd3a26cb7f7a46a
Kin <winterheat@gmail.com>
2016-03-06 03:24:56 -0800

    ok

--- tryCounterElements.py
+++ tryCounterElements.py
@@ -18,3 +18,5 @@ print(counter.elements())
 print([i for i in counter.elements()])
 
 print(reduce(lambda a, b: a + b, [[k] * v for (k, v) in counter.items()]))
+
+print([type(i) for i in counter.most_common(2)])
