25180c83a815d0ec33308393967d4bd2282529ff
Kin <winterheat@gmail.com>
2016-03-06 01:43:51 -0800

    ok

--- tryCounter.py
+++ tryCounter.py
@@ -2,8 +2,8 @@ import collections
 
 counter = collections.Counter(['hello', 'world', 'hello', 'hi', 'hello', 'hi'])
 
-print counter
+print(counter)
 
-print counter.most_common(1)
-print counter.most_common(2)
-print counter.least_common(2)
+print(counter.most_common(1))
+print(counter.most_common(2))
+print(counter.least_common(2))
